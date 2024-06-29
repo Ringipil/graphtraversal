61d715380cf680462e1d5024385a4ca65ccefa32
Ringipil <153096214+Ringipil@users.noreply.github.com>
2024-06-29 19:44:14 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,4 @@
+/*
 #include "mainwindow.h"
 #include "ui_MainWindow.h"
 
@@ -55,4 +56,56 @@ void MainWindow::on_bfsButton_clicked() {
     };
 
     graph.bfs(startNode, stepCallback);
+}
+*/
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //graph = new Graph(false);  // Assume false means undirected
+    scene = new QGraphicsScene(this);
+    view = ui->graphicsView;
+    view->setScene(scene);
+    stepsList = ui->stepsListWidget;
+
+    // Connect buttons to slots
+    connect(ui->addVertexButton, &QPushButton::clicked, this, &MainWindow::onAddVertex);
+    connect(ui->addEdgeButton, &QPushButton::clicked, this, &MainWindow::onAddEdge);
+    connect(ui->bfsButton, &QPushButton::clicked, this, &MainWindow::onRunBFS);
+    connect(ui->dfsButton, &QPushButton::clicked, this, &MainWindow::onRunDFS);
+    connect(ui->resetGraphButton, &QPushButton::clicked, this, &MainWindow::onResetGraph);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::onAddVertex() {
+    // Implementation for adding vertex
+}
+
+void MainWindow::onAddEdge() {
+    // Implementation for adding edge
+}
+
+void MainWindow::onRunBFS() {
+    // Implementation for running BFS
+}
+
+void MainWindow::onRunDFS() {
+    // Implementation for running DFS
+}
+
+void MainWindow::onResetGraph() {
+    // Implementation for resetting graph
+}
+
+void MainWindow::updateVisualization() {
+    // Implementation for updating visualization
 }
